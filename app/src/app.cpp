a399830159c5d7d4265000fd3460d3be2ea07c3d
infopek <10saero01@gmail.com>
2025-03-08 19:51:24 +0100

    Implement Huffman encoding for a string

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -1,4 +1,5 @@
 #include <vector.h>
+#include <stack.h>
 #include <linked_list.h>
 #include <tree.h>
 #include <hash_table.h>
@@ -11,6 +12,9 @@
 #include <utility>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <stack>
 
 
 template <typename T>
@@ -57,23 +61,90 @@ void printArr(const T* arr, size_t size) {
     }
 }
 
+struct HuffmanNode {
+    char data{};
+    size_t freq{};
+
+    HuffmanNode* left;
+    HuffmanNode* right;
+
+    HuffmanNode(char data_, size_t freq_)
+        : data{ data_ }, freq{ freq_ }, left{ nullptr }, right{ nullptr } {
+    }
+    HuffmanNode(char data_, size_t freq_, HuffmanNode* left_, HuffmanNode* right_)
+        : data{ data_ }, freq{ freq_ }, left{ left_ }, right{ right_ } {
+    }
+};
+
 std::string huffmanEncode(const std::string& str) {
-    core::HashTable<char, int> freq{};
+    if (str.empty()) {
+        return "";
+    }
+
+    core::HashTable<char, size_t> freq{};
     for (const auto c : str) {
         ++freq[c];
     }
 
+    core::PQueue<size_t, HuffmanNode*> pq{};
     for (const auto& [k, v] : freq) {
-        std::cout << "kv: " << k << ' ' << v << '\n';
+        pq.insert(v, new HuffmanNode(k, v));
     }
 
-    return "";
+    while (pq.size() != 1) {
+        HuffmanNode* left = pq.peek();
+        pq.pop();
+
+        HuffmanNode* right = pq.peek();
+        pq.pop();
+
+        size_t sum = left->freq + right->freq;
+        HuffmanNode* combined = new HuffmanNode(' ', sum, left, right);
+        pq.insert(sum, combined);
+    }
+
+    // Iterative preorder so that I can do this inside this method
+    core::Vector<std::string> codes(256);   // ascii
+    core::Stack<std::pair<HuffmanNode*, std::string>> stack{};
+
+    HuffmanNode* root = pq.peek();
+    stack.push({ root, "" });
+    while (!stack.empty()) {
+        auto [node, code] = stack.peek();
+        stack.pop();
+
+        if (!node->left && !node->right) {
+            codes[static_cast<size_t>(node->data)] = code;
+        }
+
+        if (node->left) {
+            stack.push({ node->left, code + "0" });
+        }
+        if (node->right) {
+            stack.push({ node->right, code + "1" });
+        }
+    }
+
+    // Join the codes to get final encoded msg
+    size_t encodedLength = 0;
+    for (const auto c : str) {
+        encodedLength += codes[static_cast<size_t>(c)].length();
+    }
+
+    std::string res{};
+    res.reserve(encodedLength);
+    for (const auto c : str) {
+        res += codes[static_cast<size_t>(c)] + " ";
+    }
+
+    return res;
 }
 
 int main() {
     try {
         std::string str{ "BCCABBDDAECCBBAEDDCC" };
-        std::cout << huffmanEncode(str);
+        std::string res = huffmanEncode(str);
+        std::cout << res;
     }
     catch (const std::exception& ex) {
         std::cerr << "Exception occurred: " << ex.what();
